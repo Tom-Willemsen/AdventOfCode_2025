d66b8d12a648df7335a0f630211f552f511ddd36
Tom Willemsen <tomwillemsen1995@gmail.com>
2025-11-29 10:12:51 +0000

    very important stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import { NoSsr } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import Link from "next/link";
 
 const geistMono = Geist_Mono({
 	variable: "--font-geist-mono",
@@ -24,10 +26,20 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={`${geistMono.variable} antialiased`}>
 				<Script src="enable-threads.js" strategy="beforeInteractive" />
-				<div className="flex place-content-center bg-gradient-to-r from-[#c9040b] via-[#b0040a] to-[#c9040b]">
-					<h1 className="font-mono p-4 font-bold text-align-center text-l sm:text-xl text-white">
+				<div className="flex items-center bg-gradient-to-r from-[#c9040b] via-[#b0040a] to-[#c9040b] w-screen">
+					<h1 className="font-mono p-4 font-bold text-center text-l sm:text-xl text-white grow">
 						🎄✨🎄 Advent of Code 2025 🎄✨🎄
 					</h1>
+					<Link
+						href={"https://github.com/Tom-Willemsen/AdventOfCode_2025"}
+						target="_blank"
+					>
+						<GitHubIcon
+							className="float-right mx-4"
+							fontSize="large"
+							htmlColor="#ffffff"
+						/>
+					</Link>
 				</div>
 				<hr />
 				<div className="flex w-screen p-4 place-content-center align-content-center text-align-center">
--- app/webworker.ts
+++ app/webworker.ts
@@ -3,30 +3,30 @@ import init, { initThreadPool, run_day } from "@/aoc2025_wasm/pkg/aoc2025_wasm";
 let init_done = false;
 
 function warmup() {
-    for (let day = 1; day <= 12; day++) {
-        run_day(day, "");
-    }
+	for (let day = 1; day <= 12; day++) {
+		run_day(day, "");
+	}
 }
 
 function post_answer(day: number, input: string): void {
-    const startTime = performance.now();
-    const workerResult = run_day(day, input);
-    const endTime = performance.now();
-    postMessage([workerResult, endTime - startTime]);
+	const startTime = performance.now();
+	const workerResult = run_day(day, input);
+	const endTime = performance.now();
+	postMessage([workerResult, endTime - startTime]);
 }
 
 // biome-ignore lint/suspicious/noGlobalAssign: see https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
 onmessage = (msg) => {
-    const [day, input] = msg.data;
-    if (!init_done) {
-        init().then(() => {
-            initThreadPool(navigator.hardwareConcurrency).then(() => {
-                warmup();
-                init_done = true;
-                post_answer(day, input);
-            });
-        });
-    } else {
-        post_answer(day, input);
-    }
+	const [day, input] = msg.data;
+	if (!init_done) {
+		init().then(() => {
+			initThreadPool(navigator.hardwareConcurrency).then(() => {
+				warmup();
+				init_done = true;
+				post_answer(day, input);
+			});
+		});
+	} else {
+		post_answer(day, input);
+	}
 };
