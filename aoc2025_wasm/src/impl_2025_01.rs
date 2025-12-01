72d8919cb22d50c112f9dec4bbc48e1b6ae1f6a2
Tom Willemsen <tomwillemsen1995@gmail.com>
2025-12-01 06:37:58 +0000

    2025-01

--- aoc2025_wasm/src/impl_2025_01.rs
+++ aoc2025_wasm/src/impl_2025_01.rs
@@ -1,33 +1,44 @@
-use ahash::AHashMap;
-use anyhow::{Result, anyhow};
-use itertools::Itertools;
-use std::iter::zip;
+use anyhow::{Result, anyhow, bail};
+use num::traits::Euclid;
 
 fn calculate(raw_inp: &str) -> Result<(i32, i32)> {
-    let mut left = vec![];
-    let mut right = vec![];
+    let mut p1 = 0;
+    let mut p2 = 0;
+    let mut rot = 50;
 
     for line in raw_inp.lines() {
-        let (l, r) = line.split_once("   ").ok_or(anyhow!("bad format"))?;
-        left.push(l.parse()?);
-        right.push(r.parse()?);
+        let (dir, num) = line
+            .split_at_checked(1)
+            .ok_or_else(|| anyhow!("invalid line format"))?;
+
+        let num = num.parse::<i32>()?;
+
+        let (full_turns, num) = num.div_rem_euclid(&100);
+
+        p2 += full_turns;
+
+        match dir {
+            "L" => {
+                rot += num;
+                if rot >= 100 {
+                    p2 += 1;
+                }
+            }
+            "R" => {
+                if rot - num <= 0 && rot > 0 {
+                    p2 += 1;
+                }
+                rot -= num;
+            }
+            _ => bail!("invalid line format"),
+        }
+
+        rot = rot.rem_euclid(100);
+
+        if rot == 0 {
+            p1 += 1;
+        }
     }
-
-    left.sort_unstable();
-    right.sort_unstable();
-
-    let right_count: AHashMap<i32, i32> = right
-        .iter()
-        .dedup_with_count()
-        .map(|(count, v)| (*v, count as i32))
-        .collect();
-
-    let p1 = zip(&left, &right).map(|(l, r)| (l - r).abs()).sum();
-
-    let p2 = left
-        .into_iter()
-        .map(|l| l * right_count.get(&l).unwrap_or(&0))
-        .sum();
 
     Ok((p1, p2))
 }
@@ -39,8 +50,18 @@ pub fn run_2025_01(inp: &str) -> Result<String> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    const EXAMPLE_DATA: &str = include_str!("../inputs/examples/2025_01");
+    const REAL_DATA: &str = include_str!("../inputs/real/2025_01");
+
     #[test]
-    fn test_one_plus_one() {
-        assert_eq!(1 + 1, 2);
+    fn test_example() {
+        assert_eq!(calculate(&EXAMPLE_DATA).unwrap(), (3, 6));
+    }
+
+    #[test]
+    fn test_real() {
+        assert_eq!(calculate(&REAL_DATA).unwrap(), (1118, 6289));
     }
 }
