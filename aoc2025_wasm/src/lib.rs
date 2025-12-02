b5e9387cd14b7efbfb078de91fee62fa71ba3e21
Tom Willemsen <tomwillemsen1995@gmail.com>
2025-12-02 06:37:35 +0000

    2025-02

--- aoc2025_wasm/src/lib.rs
+++ aoc2025_wasm/src/lib.rs
@@ -1,4 +1,5 @@
 mod impl_2025_01;
+mod impl_2025_02;
 
 pub use wasm_bindgen_rayon::init_thread_pool;
 
@@ -9,6 +10,7 @@ use wasm_bindgen::prelude::*;
 pub fn run_day(day: u32, input: &str) -> String {
     let res = match day {
         1 => impl_2025_01::run_2025_01(input),
+        2 => impl_2025_02::run_2025_02(input),
         _ => Err(anyhow!("No solution for that day yet")),
     };
 
