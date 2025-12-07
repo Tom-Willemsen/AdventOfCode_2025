48a95b1834e561c815a8f4a82c77898e383083c8
Tom Willemsen <tomwillemsen1995@gmail.com>
2025-12-07 07:00:06 +0000

    🧑‍🔬

--- aoc2025_wasm/src/impl_2025_07.rs
+++ aoc2025_wasm/src/impl_2025_07.rs
@@ -1,43 +1,9 @@
-use std::collections::HashMap;
-
 use anyhow::{Context, Result};
 use ndarray::Array2;
 
 use crate::grid_util::make_byte_grid;
 
-fn calculate_p1(mut grid: Array2<u8>) -> u64 {
-    let mut ans = 0;
-    for y in 1..grid.dim().0 {
-        for x in 0..grid.dim().1 {
-            if grid.get((y - 1, x)) == Some(&b'|') || grid.get((y - 1, x)) == Some(&b'S') {
-                if grid.get((y, x)) == Some(&b'.') {
-                    *grid.get_mut((y, x)).expect("invalid indices") = b'|';
-                } else if grid.get((y, x)) == Some(&b'^') {
-                    ans += 1;
-                    if let Some(left) = grid.get_mut((y, x - 1)) {
-                        *left = b'|';
-                    }
-                    if let Some(right) = grid.get_mut((y, x + 1)) {
-                        *right = b'|';
-                    }
-                }
-            }
-        }
-    }
-    ans
-}
-
-fn many_worlds(
-    grid: &Array2<u8>,
-    mut y: usize,
-    x: usize,
-    cache: &mut HashMap<(usize, usize), u64>,
-) -> u64 {
-    if let Some(cached_result) = cache.get(&(y, x)) {
-        return *cached_result;
-    }
-
-    let original_y = y;
+fn many_worlds(grid: &Array2<u8>, mut y: usize, x: usize, cache: &mut Array2<u64>) -> u64 {
     while grid.get((y + 1, x)) == Some(&b'.') {
         y += 1;
     }
@@ -46,23 +12,36 @@ fn many_worlds(
         return 1;
     }
 
+    if let Some(cached_result) = cache.get((y, x))
+        && cached_result != &0
+    {
+        return *cached_result;
+    }
+
     let result = many_worlds(grid, y + 1, x - 1, cache) + many_worlds(grid, y + 1, x + 1, cache);
-    cache.insert((original_y, x), result);
+    if let Some(cached_result) = cache.get_mut((y, x)) {
+        *cached_result = result;
+    }
     result
 }
 
-fn calculate_p2(grid: &Array2<u8>) -> Result<u64> {
-    let mut cache = HashMap::<(usize, usize), u64>::default();
-    grid.indexed_iter()
+fn calculate(grid: &Array2<u8>) -> Result<(usize, u64)> {
+    let mut cache = Array2::from_elem(grid.dim(), 0);
+
+    let p2 = grid
+        .indexed_iter()
         .find(|&(_, elem)| elem == &b'S')
         .map(|((y, x), _)| many_worlds(grid, y, x, &mut cache))
-        .context("can't find start")
+        .context("can't find start")?;
+
+    let p1 = cache.into_iter().filter(|&n| n > 0).count();
+
+    Ok((p1, p2))
 }
 
 pub fn run_2025_07(inp: &str) -> Result<String> {
     let grid = make_byte_grid(inp)?;
-    let p2 = calculate_p2(&grid)?;
-    let p1 = calculate_p1(grid);
+    let (p1, p2) = calculate(&grid)?;
     Ok(format!("{p1}\n{p2}"))
 }
 
@@ -75,26 +54,32 @@ mod tests {
 
     #[test]
     fn test_example_p1() {
-        assert_eq!(calculate_p1(make_byte_grid(EXAMPLE_DATA).unwrap()), 21);
+        assert_eq!(
+            calculate(&make_byte_grid(EXAMPLE_DATA).unwrap()).unwrap().0,
+            21
+        );
     }
 
     #[test]
     fn test_example_p2() {
         assert_eq!(
-            calculate_p2(&make_byte_grid(EXAMPLE_DATA).unwrap()).unwrap(),
+            calculate(&make_byte_grid(EXAMPLE_DATA).unwrap()).unwrap().1,
             40
         );
     }
 
     #[test]
     fn test_real_p1() {
-        assert_eq!(calculate_p1(make_byte_grid(REAL_DATA).unwrap()), 1711);
+        assert_eq!(
+            calculate(&make_byte_grid(REAL_DATA).unwrap()).unwrap().0,
+            1711
+        );
     }
 
     #[test]
     fn test_real_p2() {
         assert_eq!(
-            calculate_p2(&make_byte_grid(REAL_DATA).unwrap()).unwrap(),
+            calculate(&make_byte_grid(REAL_DATA).unwrap()).unwrap().1,
             36706966158365
         );
     }
